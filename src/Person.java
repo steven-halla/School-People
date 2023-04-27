faa3097f9bfd55bc5981ea55c6aadc6de8c36b3f
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-04-26 18:19:46 -0700

    Person class complete

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,56 @@
+
 public class Person {
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private String emailAddress;
+
+    // Constructor
+    public Person(String name, String address, String phoneNumber, String emailAddress) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", address=" + address + ", phoneNumber=" + phoneNumber
+                + ", emailAddress=" + emailAddress + "]";
+    }
 }
